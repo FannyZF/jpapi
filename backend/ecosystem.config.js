37131354b627852abbb0c4489d84d01a59bf2ece
FannyZF <fanny.yangzhenfan@gmail.com>
2026-05-27 11:11:12 +0800

    Remove hardcoded PORT from ecosystem config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -14,7 +14,6 @@ module.exports = {
       max_memory_restart: "500M",
       env: {
         NODE_ENV: "production",
-        PORT: 3000,
       },
       error_file: "../logs/err.log",
       out_file: "../logs/out.log",
